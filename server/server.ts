007fdd19c68d7e72fa5bb2ad61ad62d91d82f44a
ppppppsmash <pppppp.work@gmail.com>
2023-11-26 23:23:57 +0900

    feat: create node server

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,34 @@
-console.log('Hello World!!');
+import * as http from 'http';
+import * as WebSocket from 'ws';
+
+const hostname = 'localhost';
+const port = 9999;
+
+const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws: WebSocket, req: http.IncomingMessage) => {
+
+  ws.on('message', (message: string) => {
+    console.log(`Received message: ${message}`);
+
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
